896197f5d94c1cee76842449a04c60c932fe5bec
Samith R Gowda <141106865+Samithsr@users.noreply.github.com>
2025-09-26 18:01:14 +0530

    in menu add increase and decrease items

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({id, name, description, image, price}) => {
 
-    
+
+    const [itemcount, setItemCount] = useState(0);
+
   return (
     <div className='food-item'>
        <div className="foof-item-img-container">
         <img className='food-item-image' src={image} alt="" />
+
+        {!itemcount
+         > <img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" srcset="" />
+         : <div className='food-item-counter' >
+
+         </div>
+        
+        }
+
        </div>
        <div className="food-item-info">
         <div className="food-item-name-rating">
